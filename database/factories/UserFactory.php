a1d62607a0a511df89374daa5b83dad9a7a4455b
MERGEN NURYYEV <m.nuryyev@iqfulfillment.com>
2022-01-12 08:20:26 +0400

    new model and migrations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use phpDocumentor\Reflection\Element;
 
 class UserFactory extends Factory
 {
@@ -20,6 +22,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'verified' => $verified = $this->faker->randomElement([User::VERIFIED_USER,User::UNVERIFIED_USER]),
+            'verified_token' => $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
         ];
     }
 
